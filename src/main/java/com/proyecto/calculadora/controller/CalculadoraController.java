7b640c215a83d4428d05a09b3369460d39e56e25
Pricecito <matas.gutierrez.1254@gmail.com>
2024-12-04 11:26:47 -0500

    Welcome

--- src/main/java/com/proyecto/calculadora/controller/CalculadoraController.java
+++ src/main/java/com/proyecto/calculadora/controller/CalculadoraController.java
@@ -1,15 +1,25 @@
 package com.proyecto.calculadora.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("calculadora")
 public class CalculadoraController {
     @GetMapping
-    public String index() {
-        return "calculadora/index";
+    public String index(Model modelo) {
+        modelo.addAttribute("title", "BIENVENIDO A LA CALCULADORA");
+        return "calculadora/form";
+    }
+
+    @PostMapping
+    public String postMethodName(@RequestParam("nombreUser") String name, Model modelo) {
+        modelo.addAttribute("name", name);
+        return "calculadora/welcome";
     }
 
 }
