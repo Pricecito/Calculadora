f68f29033eb89085a898da218f3fb88db347b203
Pricecito <matias.gutierrez.1254@gmail.com>
2025-06-28 23:13:37 -0500

    finish project

--- src/main/java/com/proyecto/calculadora/controller/CalculadoraController.java
+++ src/main/java/com/proyecto/calculadora/controller/CalculadoraController.java
@@ -16,10 +16,31 @@ public class CalculadoraController {
         return "calculadora/form";
     }
 
-    @PostMapping
-    public String postMethodName(@RequestParam("nombreUser") String name, Model modelo) {
+    @PostMapping("/login")
+    public String login(@RequestParam("nombreUser") String name, Model modelo) {
         modelo.addAttribute("name", name);
         return "calculadora/welcome";
     }
 
+    @PostMapping("/operar")
+    public String suma(@RequestParam("num1") int n1, @RequestParam("num2") int n2,
+            @RequestParam("operation") String operation, Model modelo) {
+        Object result;
+        switch (operation) {
+            case "suma":
+                result = n1 + n2;
+                break;
+            case "resta":
+                result = n1 - n2;
+                break;
+            case "multiplicacion":
+                result = n1 * n2;
+                break;
+            default:
+                result = Math.round(n1 / n2);
+        }
+        ;
+        modelo.addAttribute("result", result);
+        return "calculadora/welcome";
+    }
 }
